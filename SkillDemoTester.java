53870a6b8ec6d9de50346211897c7bad819277b2
Niklichang <nhchang@ucsd.edu>
2022-02-04 13:39:14 -0800

    Update SkillDemoTester.java

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -1,11 +1,9 @@
-import org.junit.Test;
-
 import static org.junit.Assert.*;
 import org.junit.*;
 
 public class SkillDemoTester {
     @Test
-    public void testSubtract{
+    public void testSubtract(){
         assertEquals(2, SkillDemo.subtract(4, 3));// fails
     }
 }
